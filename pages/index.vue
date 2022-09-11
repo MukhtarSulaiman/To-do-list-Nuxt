5cb242570484f79a16424b4bdcc0c58d84e40e8b
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-09-12 00:39:03 +0200

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<main>
+		<h1>Task Monitor</h1>
+		<p>Your list of tasks</p>
+		<div class="create-new">
+			<input 
+				type="text" 
+				v-model="newTask" 
+				placeholder="Add a new task" 
+				@keypress.enter="addTask">
+			<button @click="addTask">Add</button>
+		</div>
+		<div class="tasks">
+			<!-- component -->
+		</div>
+	</main>
+
+</template>
+
+<script>
+export default {
+	data (){
+		return {
+			newTask: null,
+		}
+	},
+	methods: {
+		addTask() {
+			if(this.newTask) {
+				this.$store.commit('ADD_TASK', this.newTask)
+			}
+		}
+	}
+}
+</script>
---
+++ store/index.js
@@ -0,0 +1,11 @@
+export const mutations = {
+      ADD_TAST(state, task) {
+            state.tasks = [{content: task, done: false, ...state.tasks}]
+      },
+      REMOVE_TASK(state, task) {
+            state.tasks.splice(state.tasks.indexOf(task, 1))
+      },
+      TOGGLE_TASK(state, task) {
+            task.done = !task.done
+      }
+}
